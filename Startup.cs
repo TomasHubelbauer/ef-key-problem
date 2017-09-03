e50b88f0a77cd7f232da68f4d6c95fff785f3e06
Tomáš Hübelbauer <tomas@hubelbauer.net>
2017-09-03 09:20:49 +0200

    Enable sensitive data logging to reveal Ids

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace ef_key_problem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<EfKeyContext>(opt => opt.UseInMemoryDatabase("EfKey"));
+            services.AddDbContext<EfKeyContext>(opt => opt.UseInMemoryDatabase("EfKey").EnableSensitiveDataLogging());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
